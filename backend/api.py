9f9ea87355e49bc04bb84c0574f4c40b905a27a7
MayukhBanerjee <mayukh647@gmail.com>
2026-04-06 03:55:01 +0530

    Added Middleware to the backend

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-
+from fastapi.middleware.cors import CORSMiddleware
 from main import rag_chain
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QuerryRequest(BaseModel):
     query:str
